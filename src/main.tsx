eecbcafa820aabfc6fb445369ede1554f733cb8b
Annabelle <annabelle.braye@gmail.com>
2024-05-07 00:17:39 +0200

    chore(chore): Install app, add tialwind, move files and folders, remove svg.

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Profile from './pages/Profile/index.tsx'
+import './styles/main.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Profile />
+  </React.StrictMode>,
+)
