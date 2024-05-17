e64354d34fa8deb83ecb6a15868253e3441b6d67
Annabelle <annabelle.braye@gmail.com>
2024-05-17 11:29:22 +0200

    feat(cards): Creation of calorie, protein, carbonhydrate and lipid card.

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,23 @@
+export type CardType = {
+  image: string,
+  value: string,
+  name: string
+}
+
+const Card = ({
+  image,
+  value,
+  name
+}: CardType) => {
+  return (
+    <div className="flex items-center w-full rounded-md bg-light-grey">
+      <img src={image} className="m-8 mr-6" width={60} height={60} />
+      <div className="flex flex-col">
+        <h3 className="text-secondary-color text-xl font-bold">{value}</h3>
+        <p className="text-legend-grey text-sm font-medium">{name}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
