9007771a8fc527b65e542ea10006477fca03fd69
Annabelle <annabelle.braye@gmail.com>
2024-05-27 16:28:42 +0200

    refactor(provider): remove provider.

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import Profile from './pages/Profile/index.tsx'
 import './styles/main.css'
 import Header from './components/Header/index.tsx'
 import Footer from './components/Footer/index.tsx'
-import { UserProvider } from './utils/providers/userProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <UserProvider>
-      <Header />
-      <Profile />
-      <Footer />
-    </UserProvider>
+    <Header />
+    <Profile />
+    <Footer />
   </React.StrictMode>,
 )
